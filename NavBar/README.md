ec7d2ea929b5e43e1cd79645e0dcd07e4312e1f9
Qazi Muhammad Ghous Afaq <qaziafaq1212@gmail.com>
2026-04-18 03:52:13 -0700

    Delete NavBar directory

--- NavBar/README.md
+++
@@ -1,2 +0,0 @@
-# My-First-Upload
-This is my first Upload in Bano Qabil Web Development 
--- NavBar/navbar.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const enrollBtn = document.getElementById("NavbarEnrollBtn");
-
-    enrollBtn.addEventListener("click", function(){
-        window.location.href = "../Form/Enroll.html";
-    });
-});
